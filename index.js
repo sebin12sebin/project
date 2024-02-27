fc61fc01e84987222530531ca00476d240b48a30
sebin12sebin <140684273+sebin12sebin@users.noreply.github.com>
2024-02-27 22:28:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-var user=require("./routes/route")
+var user=require("./route")
 
 const app = express();
 
